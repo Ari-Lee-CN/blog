1f30b48c61f289e4e5607ccb7cd4edd0bf982100
arilee <arileecn@email.com>
2022-05-21 19:46:58 +0800

    重新整理了一遍

--- jirengu/学习笔记/Typescript/hello.ts
+++
@@ -1,7 +0,0 @@
-function sayHello(person: string) {
-    return 'Hello, ' + person;
-}
-
-let user = 'Tom';
-console.log(sayHello(user));
-//tsc hello.js 编译为js文件
--- 其他笔记/GitHub Flavored Markdown.md
+++
@@ -1,2 +0,0 @@
-# GitHub Flavored Markdown
-
